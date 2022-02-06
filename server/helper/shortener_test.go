afd846267da46064109a40af914dc0a7c40a2695
truecoder34 <vladplotnikov34@gmail.com>
2022-02-06 15:02:38 +0300

    En-/De-coder covering with positive unit tests

--- server/helper/shortener_test.go
+++ server/helper/shortener_test.go
@@ -5,16 +5,47 @@ import (
 	"testing"
 )
 
+type encodeTest struct {
+	arg      uint64
+	expected string
+}
+
+var encodeTests = []encodeTest{
+	{7, "h"},
+	{555555, "JGuc"},
+	{10000000000, "KY8U4k"},
+	{0, ""},
+}
+
+type decodeTest struct {
+	arg      string
+	expected uint64
+}
+
+var decodeTests = []decodeTest{
+	{"QQQ", 164094},
+	{"12Z76", 871288285},
+	{"godevblogusinggomodules", 6156118248968608582},
+}
+
 func TestEncode(t *testing.T) {
-	var data2Encode uint64 = 7
-
-	encoded_url := Encode(data2Encode)
-	expected, decodeErr := Decode(encoded_url)
-	if decodeErr != nil {
-		log.Fatalln("Error generating QR code. ", decodeErr)
+	for idx, testData := range encodeTests {
+		encoded_number := Encode(testData.arg)
+		if encoded_number != testData.expected {
+			t.Errorf("[ TEST %d ] : Output %q not equal to expected %q", idx, encoded_number, testData.expected)
+		}
 	}
+}
 
-	if data2Encode != expected {
-		t.Errorf("got %q, wanted %q", expected, 7)
+func TestDecode(t *testing.T) {
+	for idx, testData := range decodeTests {
+		number, decodeErr := Decode(testData.arg)
+		if decodeErr != nil {
+			log.Fatalln("[ERROR] Decoder() error : ", decodeErr)
+		}
+
+		if number != testData.expected {
+			t.Errorf("[ TEST %d ] : Output %q not equal to expected %q", idx, number, testData.expected)
+		}
 	}
 }
--- server/main.go
+++ server/main.go
@@ -1,7 +1,20 @@
 package main
 
-//import "OMUS/server/app"
+import (
+	"OMUS/server/helper"
+	"log"
+)
 
 func main() {
 	//app.StartApp()
+	var data2Encode uint64 = 0
+	encoded_number := helper.Encode(data2Encode)
+	log.Printf("[RESULT] Enocde() - %s", encoded_number)
+
+	decode, err := helper.Decode("godevblogusinggomodules")
+	if err != nil {
+		log.Fatalln("")
+	}
+	log.Printf("[RESULT] Decode() - %d", decode)
+
 }
