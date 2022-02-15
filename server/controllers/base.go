cb3963ce9ee6b7044cecce40adfdc6cf09fbdd1d
truecoder34 <vladplotnikov34@gmail.com>
2022-02-15 23:49:53 +0300

    conrollers

--- server/controllers/base.go
+++ server/controllers/base.go
@@ -5,3 +5,17 @@ package controllers
 	routes initialization
 	server start up
 */
+
+import (
+	// "fmt"
+	// "log"
+	// "net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/jinzhu/gorm"
+)
+
+type Server struct {
+	DB     *gorm.DB
+	Router *mux.Router
+}
